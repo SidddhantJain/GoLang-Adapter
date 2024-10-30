3dd53c65dd18d905c95f96f3c8f8b58f300e0cd4
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:24:38 +0530

    product_models.go

--- models/product_models.go
+++ models/product_models.go
@@ -1,1 +1,8 @@
 package models
+
+type Product struct {
+    ProductID string  `json:"product_id"`
+    Name      string  `json:"name"`
+    Price     float64 `json:"price"`
+    Stock     int     `json:"stock"`
+}
