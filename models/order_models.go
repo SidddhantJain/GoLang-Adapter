73dbd3dc8cbcfd66fffe12517afd1c8420bc09ff
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:24:16 +0530

    order_models.go

--- models/order_models.go
+++ models/order_models.go
@@ -1,1 +1,9 @@
 package models
+
+type Order struct {
+    OrderID     string  `json:"order_id"`
+    ProductID   string  `json:"product_id"`
+    Quantity    int     `json:"quantity"`
+    Price       float64 `json:"price"`
+    Status      string  `json:"status"`
+}
