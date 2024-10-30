545ba2fa0666348056c269f16185cc6423e51fef
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:22:24 +0530

    order_api.go

--- api/order_api.go
+++ api/order_api.go
@@ -1,1 +1,21 @@
 package api
+
+import "net/http"
+
+type OrderAPI struct {
+    BaseURL string
+    Client  *http.Client
+}
+
+func NewOrderAPI(baseURL string) *OrderAPI {
+    return &OrderAPI{
+        BaseURL: baseURL,
+        Client:  &http.Client{},
+    }
+}
+
+// Placeholder function to create an order
+func (o *OrderAPI) CreateOrder(orderData map[string]interface{}) error {
+    // Implementing order creation logic here
+    return nil
+}
