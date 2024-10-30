82441537f5274f87989349c6d06a59939a6aba1e
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:27:25 +0530

    Update order_service.go

--- services/order_service.go
+++ services/order_service.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+    "adapter-project/api"
+    "adapter-project/models"
+)
+
+type OrderService struct {
+    api *api.OrderAPI
+}
+
+func NewOrderService(baseURL string) *OrderService {
+    return &OrderService{
+        api: api.NewOrderAPI(baseURL),
+    }
+}
+
+
+/*
+func (s *OrderService) CreateOrder(orderData models.Order) error {
+    // no logic for creation of order - to be redirected using route_initializer.go
+    return s.api.CreateOrder(map[string]interface{}{
+        "product_id": orderData.ProductID,
+        "quantity":   orderData.Quantity,
+    })
+}
+*/
