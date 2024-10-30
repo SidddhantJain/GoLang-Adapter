5c7cf4b6604b1c07c31b63bc25f9092ae08e8631
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:23:04 +0530

    product_api.go

--- api/product_api.go
+++ api/product_api.go
@@ -1,1 +1,21 @@
 package api
+
+import "net/http"
+
+type ProductAPI struct {
+    BaseURL string
+    Client  *http.Client
+}
+
+func NewProductAPI(baseURL string) *ProductAPI {
+    return &ProductAPI{
+        BaseURL: baseURL,
+        Client:  &http.Client{},
+    }
+}
+
+// Placeholder function for product retrieval
+func (p *ProductAPI) GetProduct(productID string) (map[string]interface{}, error) {
+    // product retrieval logic to be implemented
+    return nil, nil
+}
