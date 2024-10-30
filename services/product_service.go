b0202268783800c36e0ab67d7d4f34f5d3aa7a8a
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:29:36 +0530

    Update product_service.go

--- services/product_service.go
+++ services/product_service.go
@@ -1,1 +1,28 @@
 package services
+
+import (
+    "adapter-project/api"
+    "adapter-project/models"
+)
+
+type ProductService struct {
+    api *api.ProductAPI
+}
+
+func NewProductService(baseURL string) *ProductService {
+    return &ProductService{
+        api: api.NewProductAPI(baseURL),
+    }
+}
+
+func (s *ProductService) GetProduct(productID string) (models.Product, error) {
+    productData, err := s.api.GetProduct(productID)
+    if err != nil {
+        return models.Product{}, err
+    }
+    // Convert productData to models.Product
+    return models.Product{
+        ProductID: productID,
+        Name:      productData["name"].(string),
+    }, nil
+}
