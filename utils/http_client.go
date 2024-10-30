c76f260c2c8984b8c4d1d19b19d51bddf6e84810
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:30:00 +0530

    Update http_client.go

--- utils/http_client.go
+++ utils/http_client.go
@@ -1,1 +1,16 @@
 package utils
+
+import (
+    "bytes"
+    "encoding/json"
+    "net/http"
+)
+
+func PostRequest(url string, payload interface{}) (*http.Response, error) {
+    jsonData, err := json.Marshal(payload)
+    if err != nil {
+        return nil, err
+    }
+
+    return http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+}
