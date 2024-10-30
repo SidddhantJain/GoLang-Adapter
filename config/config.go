654d69c2be67b25dc2fd75606f5d077fda196396
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-10-30 22:23:46 +0530

    config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,16 @@
 package config
+
+import "os"
+
+var (
+    BaseURL       = getEnv("BASE_URL", "https://api.definedge.com")
+    AuthEndpoint  = getEnv("AUTH_ENDPOINT", "/auth/login")
+    OrderEndpoint = getEnv("ORDER_ENDPOINT", "/placeorder")
+)
+
+func getEnv(key, fallback string) string {
+    if value, exists := os.LookupEnv(key); exists {
+        return value
+    }
+    return fallback
+}
