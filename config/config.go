1af83e7e99f0820e60d6a44ab8baa398e0244746
Siddhant Mishrikotkar <149305963+SidddhantJain@users.noreply.github.com>
2024-11-23 08:13:37 +0530

    Update config.go
    Addition of descriptio

--- config/config.go
+++ config/config.go
@@ -2,15 +2,17 @@ package config
 
 import "os"
 
+// Configuration for API endpoints and base URL.
 var (
-    BaseURL       = getEnv("BASE_URL", "https://api.definedge.com")
-    AuthEndpoint  = getEnv("AUTH_ENDPOINT", "/auth/login")
-    OrderEndpoint = getEnv("ORDER_ENDPOINT", "/placeorder")
+	BaseURL       = getEnv("BASE_URL", "https://api.definedge.com")    // Default API base URL
+	AuthEndpoint  = getEnv("AUTH_ENDPOINT", "/auth/login")            // Endpoint for authentication
+	OrderEndpoint = getEnv("ORDER_ENDPOINT", "/placeorder")           // Endpoint for placing orders
 )
 
+// getEnv fetches the value of an environment variable or returns a fallback value if not set.
 func getEnv(key, fallback string) string {
-    if value, exists := os.LookupEnv(key); exists {
-        return value
-    }
-    return fallback
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
 }
